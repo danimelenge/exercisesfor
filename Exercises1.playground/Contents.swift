052e337e5a6599773186e27cdda17f08e66a17b2
daniel melenge <danielmelenge@gmail.com>
2023-04-13 13:54:22 -0500

    .Da

---
+++ Exercises1.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+// Programa que pide los numeros del 1 al 20.
+
+for num in 1...20 {
+  if num % 2 == 0 {
+    print(num)
+  }
+}
+
+// Programa que pide los numeros impares del 1 al 20.
+
+for num in 1...20 {
+  if num % 2 != 0 {
+    print(num)
+  }
+}
+
+
